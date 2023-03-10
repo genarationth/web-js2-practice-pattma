c4335cf758fe61ddd9184cbb603e75627a6ea5a0
patt02 <pattarin.sine.m@gmail.com>
2023-03-10 21:57:09 +0700

    Web JS part 2

---
+++ web-js2-part2.js
@@ -0,0 +1,13 @@
+// // Part 2: New contender
+
+// // Challenge 1: Create an Age Calculator
+const birthYear = 1996;
+const futureYear = 2026;
+const myAge = futureYear - birthYear;
+console.log(myAge);
+
+// // Challenge 2: Create an information card
+let name = 'Sine';
+let location = 'Khonkaen';
+let hobby = 'playing with cats';
+console.log(`Hi, my name is ${name}. I live in ${location} and enjoy ${hobby}.`);
